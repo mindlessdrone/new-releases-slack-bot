07f7b29a86259b2dbadd9564aa96680b14718e4d
Anthony Massicci <anthony.massicci@outlook.com>
2018-10-06 17:21:13 -0400

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to new-releases-slack-bot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/new_releases_slack_bot/core_test.clj
@@ -0,0 +1,7 @@
+(ns new-releases-slack-bot.core-test
+  (:require [clojure.test :refer :all]
+            [new-releases-slack-bot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
