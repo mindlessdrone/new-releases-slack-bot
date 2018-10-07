6a31c35f5a9d924a4aeb64793a4812ca7c38b4c0
Anthony Massicci <anthony.massicci@outlook.com>
2018-10-07 16:39:54 -0400

    integrated into aws lambda

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-http "3.9.1"]
-                 [clj-spotify "0.1.7"]]
+                 [clj-spotify "0.1.7"]
+                 [com.amazonaws/aws-lambda-java-core  "1.2.0"]]
   :main ^:skip-aot new-releases-slack-bot.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/new_releases_slack_bot/core.clj
+++ src/new_releases_slack_bot/core.clj
@@ -1,6 +1,10 @@
 (ns new-releases-slack-bot.core
-  (:gen-class)
-  (:require [clojure.string :as string]))
+  (:gen-class
+    :name "com.github.mindlessdrone.NewReleasesBot"
+    :methods [^:static [handler [] String]])
+  (:require [clojure.string :as string]
+            [new-releases-slack-bot.slack :as slack]
+            [new-releases-slack-bot.spotify :refer [get-new-albums]]))
 
 (defn- new-album-releases-message [albums]
   {:text "Hello! Here are the new releases for today"
@@ -9,6 +13,17 @@
                    :text (string/join ", " (:artists album))
                    :thumb_url (:image album)})})
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args])
+(defn- send-new-releases-message []
+  (let [client (System/getenv "CLIENT_ID")
+        secret (System/getenv "CLIENT_SECRET")
+        slack-webhook (System/getenv "SLACK_WEBHOOK_URI")
+        post-to-slack (slack/make-slack-poster slack-webhook)
+        albums (get-new-albums client secret 20)]
+    (when (not-empty albums)
+      (-> albums
+          (new-album-releases-message)
+          (post-to-slack)))))
+
+(defn -handler []
+  (send-new-releases-message)
+  "ok")
