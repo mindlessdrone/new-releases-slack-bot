efdea8d0447b37736b5320b4c59996fadd800f3c
Anthony Massicci <anthony.massicci@outlook.com>
2018-10-14 16:05:01 -0400

    added album links to message

--- src/new_releases_slack_bot/spotify.clj
+++ src/new_releases_slack_bot/spotify.clj
@@ -1,11 +1,14 @@
 (ns new-releases-slack-bot.spotify
   (:require [clj-spotify.core :as spotify]
-            [clj-spotify.util :refer [get-access-token]])
-  (:import (java.time LocalDate)))
+            [clj-spotify.util :refer [get-access-token]]))
 
-(defn- current-date []
-  (-> (LocalDate/now)
-      (.toString)))
+(def spotify-link
+  (comp :spotify
+        :external_urls))
+
+(defn- extract-artist-info [artist]
+  {:name (:name artist)
+   :link (spotify-link artist)})
 
 (defn- filter-by-date [date albums]
   (filter #(= date (:release_date %)) albums))
@@ -16,22 +19,23 @@
 
 (defn- extract-album-information [album]
   {:name (:name album)
+   :link (spotify-link album)
    :artists (->> album
                  (:artists)
-                 (map :name))
+                 (map extract-artist-info))
    :image (->> album
                (:images)
-               (last)
+               (first)
                (:url))})
 
-(defn- extract-albums [response]
+(defn- extract-albums [date response]
   (->> response
       (:albums)
       (:items)
-      (filter-by-date (current-date))
+      (filter-by-date date)
       (map extract-album-information)))
 
-(defn get-new-albums [client secret amount]
+(defn get-new-albums [client secret amount date]
   (->> (get-access-token client secret)
        (spotify/get-a-list-of-new-releases (new-albums-request amount))
-       (extract-albums)))
+       (extract-albums date)))
