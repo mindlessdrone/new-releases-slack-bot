9348d063c826349dcd7bec6d150f6ab7ab44f772
Anthony Massicci <anthony.massicci@outlook.com>
2018-10-21 19:22:20 -0400

    created slack event handler

---
+++ slack-events-lambda/src/slack_events_lambda/core.clj
@@ -0,0 +1,19 @@
+(ns slack-events-lambda.core
+  (:gen-class
+    :name "NewReleasesBot.EventHandler"
+    :methods [^:static [handler [java.util.Map] java.util.Map]])
+  (:require [clojure.data.json :as json]))
+
+(defn- get-challenge [event]
+  (-> event
+      (get "body")
+      (get "challenge")))
+
+(defn -handler [event-data]
+  (println event-data)
+  (let [event (json/read-str event-data)
+        challenge (get-challenge event)]
+    (if challenge
+      {"body" challenge}
+      ("body" "ok"))))
+
---
+++ slack-events-lambda/test/slack_events_lambda/core_test.clj
@@ -0,0 +1,7 @@
+(ns slack-events-lambda.core-test
+  (:require [clojure.test :refer :all]
+            [slack-events-lambda.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
