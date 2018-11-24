35285c54feac9158dc04ac2758a46412d6547c6a
Anthony Massicci <anthony.massicci@outlook.com>
2018-11-24 14:10:17 -0500

    added json conversion, removed test

--- slack-event-handler/src/slack_event_handler/core.clj
+++ slack-event-handler/src/slack_event_handler/core.clj
@@ -1,14 +1,16 @@
 (ns slack-event-handler.core
   (:gen-class
     :methods [[handler [java.util.Map] String]])
-  (:require [sqs-module.core :as sqs]))
+  (:require [sqs-module.core :as sqs]
+            [clojure.data.json :as json]))
 
 (defn- handle-challenge [event]
   (get event "challenge"))
 
 (defn- handle-event [sqs-client queue-name event]
-
-  "ok")
+  (let [json-data (json/write-str event)]
+    (sqs/send-message-to-sqs sqs-client queue-name json-data)
+    "ok"))
 
 (defn -handler [event]
   (let [sqs-client (sqs/default-client)
